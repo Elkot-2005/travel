a6abe166072aedbe3333dd02003c281cf133f349
Elkot-2005 <mooomali15@gmail.com>
2024-09-05 20:42:21 +0300

    vv

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var co= document.querySelector(".cob1")
+
+var co1= document.querySelector(".cob2")
+
+var co2= document.querySelector(".cob3")
+
+
+    
+
+  co.onclick = function(){
+    alert("تم النسخ بنجاح ")
+  }
+
+  co1.onclick = function(){
+    alert("تم النسخ بنجاح ")
+  }
+  
+  co2.onclick = function(){
+    alert("تم النسخ بنجاح ")
+  }
+  
+  
